6d8c43c2d5b20a58f4b6fa34ad07f9af810cc812
megan0320 <45302788+megan0320@users.noreply.github.com>
2024-01-29 20:01:07 -0500

    Update gitprofile.config.ts
    Update information

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'megan0320', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
@@ -12,7 +12,7 @@ const CONFIG = {
   base: '/gitprofile/',
   projects: {
     github: {
-      display: true, // Display GitHub projects?
+      display: false, // Display GitHub projects?
       header: 'Github Projects',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
@@ -33,111 +33,149 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Local Music Website - LocalLive',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Hub for exploring local events, venues, and reviews. ',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: 'https://github.com/megan0320/LocalLive',
         },
         {
-          title: 'Project Name',
+          title: 'Census Data Visualization Web App',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Interactive web app unveiling US Census insights.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
         },
+        {
+          title: 'Legacy Data Analysis Platform',
+          description:
+            'User-friendly patient data dashboard.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+        },
+        {
+          title: 'Twitter Data Analysis on Russia-Ukraine War',
+          description:
+            'Twitter Data analysis for Russia-Ukraine war insights using LDA, LSA, & NMF. 15 words.
+',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://www.computer.org/csdl/proceedings-article/bigdataservice/2023/337900a147/1Q5ptdtFpsI',
+        },
+        {
+          title: 'Air Quality App - AirMaster',
+          description:
+            'Flutter app built air quality monitor with real-time alerts & personalized settings.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://apps.apple.com/nl/app/asuka-air-master/id1613902642',
+        },
+        {
+          title: 'IoT Control Platform',
+          description:
+            'Remote factory safety platform, real-time data comms & live info dashboards (ESP32, Node-RED, Python).',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+        },
+        
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Megan Yu',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'meganyingtzu',
+    twitter: '',
+    mastodon: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: 'https://meganyingtzu-com.stackstaging.com/',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'meganyingtzu@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1MDW7lXXvnZWrKMCzy2C3moDIpN1pfn6C/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+   'Java',
+   'R',
+   'Python',
+   'React.js',
+   'Git',
+   'Flutter',
+   'Android',
+   'JavaScript',
+   'HTML/CSS',
+   'C/C++',
+   'MySQL',
+   'Google Cloud Platform'
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Arista Networks',
+      position: 'Software Engineer Intern',
+      from: 'May 2023',
+      to: 'Aug 2023',
+      companyLink: 'https://www.arista.com/en/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Asuka Autotronics Inc.',
+      position: 'Software Engineer',
+      from: 'October 2021',
+      to: 'June 2022',
+      companyLink: 'https://www.asuka-autotronics.com/',
+    },
+    {
+      company: 'Dell Technologies',
+      position: 'Software Engineer Intern',
+      from: 'July 2021',
+      to: 'September 2021',
+      companyLink: 'https://www.dell.com/',
+    },
+    {
+      company: 'Asuka Autotronics Inc.',
+      position: 'Software Engineer',
+      from: 'October 2021',
+      to: 'June 2022',
+      companyLink: 'https://www.asuka-autotronics.com/',
     },
   ],
-  certifications: [
+  publication: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: 'Twitter Data Analysis on Russia-Ukraine War',
+      body: 'Perceiving the Ukraine-Russia Conflict: Topic Modeling and Clustering on Twitter Data',
+      year: 'September 2023',
+      link: 'https://www.computer.org/csdl/proceedings-article/bigdataservice/2023/337900a147/1Q5ptdtFpsI',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'Rensselaer Polytechnic Institute',
+      degree: 'Master's, Information Science',
+      from: '2022',
+      to: '2023',
+    },
+    {
+      institution: 'National Chung Hsing University',
+      degree: 'Bachelor's, Electrical Engineering',
       from: '2015',
       to: '2019',
     },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
   ],
   // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 3, // How many articles to display. Max is 10.
-  },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -147,11 +185,11 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'nord',
+    defaultTheme: 'bumblebee',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
